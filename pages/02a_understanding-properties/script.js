c2cd1b59100ad93b219fe4302a6819379af2e968
Mohamad_Hasan <460995mhmdhsn@gmail>
2026-05-15 16:53:09 +0300

    first animation

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,10 @@
+import gsap from "gsap";
+gsap.to(".box",{
+    opacity:1,
+    rotation:360,
+    backgroundColor:"#6366f1",
+    borderRadius:50,
+    ease:"power2.inOut",
+    duration:2,
+    delay:.5
+  })
